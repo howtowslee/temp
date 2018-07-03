0ed177366e0a3394463d547feb8f38e675095ff2
wonny <wslee@howtobiz.com>
2018-07-03 17:58:30 +0900

    [update]payroll03, 04 작업

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var pdf = require('htmltempletepdf');
 var fs = require('fs');
 var data = require('./data');
+var pandoc = require('node-pandoc');
+
+var src;
+var args;
 
 if (process.argv.length == 3) {
   name = process.argv[2];
@@ -21,3 +25,24 @@ pdf.generate({
   templatePath: html,
   data: data[name]
 })
+
+// var nameDate = new Date().getTime();
+
+// var src = './'+name+'.html';
+// var args = ['-f','html', '-t', 'docx' , '-o', nameDate + '.docx'];
+
+// // Set your callback function 
+// callback = function (err, result) {
+
+//   if (err) {
+//     console.error('Oh Nos: ',err);
+//   }
+
+//   // For output to files, the 'result' will be a boolean 'true'. 
+//   // Otherwise, the converted value will be returned. 
+//   console.log(result);
+//   return result;
+// };
+
+
+// pandoc(src, args, callback);
